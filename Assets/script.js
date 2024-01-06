3675625f558c91acf1b01e33e8598a048621eac1
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-01-06 10:42:57 -0500

    attempting to add an object and pull the current weather once search, nothing displaying on page though, only in the console. troubleshooting

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,9 @@
 var weatherAPI = "c2e41c2ad6868f94e2bbd03e123c92b8";
 var cityInput = document.getElementById("cityInput");
 var searchButton = document.getElementById("searchButton");
+var searchResults = document.getElementById("searchResults");
+var currentWeather = document.getElementById("currentWeather");
+var forecast = document.getElementById("forecast");
 
 // SEARCH BUTTON - click
 searchButton.addEventListener("click", function() {
@@ -14,15 +17,38 @@ console.log("clicked search");
     .then(data => {
      // Shows data for the city user searched
         console.log(data);
+        
     })
 // COME BACK TO THIS, I WANT TO ADD AN ERROR WINDOW/ALERT, NOT INCLUDED IN README
     .catch(error => {
 
         console.log(error);
     });
-    
-
 });
 
+var weatherData = [];
 
+function fetchWeatherData (city) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=c2e41c2ad6868f94e2bbd03e123c92b8`)
+    .then(response => response.json())
+    .then(data => {
+        // pulling required weather information
+        var cityName = data.name;
+        var temperature = data.main.temp; 
+        var humidity = data.main.humidity;
+
+        var currentWeather = {
+            city: cityName, 
+            temperature: temperature, 
+            humidity: humidity,
+        };
+
+        weatherData.push(currentWeather);
+
+    })
+
+    .catch(error => {
+        console.log("Error fetching weather data", error)
+    });
+}
   
